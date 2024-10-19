68c5931eee3d1aa0fe38d43a2afaa3932e3df028
Manav <manav18gadhiya@gmail.com>
2024-10-19 16:44:39 +0530

    nft-deployer frontend component

---
+++ frontend/app/api/files/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse, NextRequest } from "next/server";
+import { pinata } from "@/lib/config";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request: NextRequest) {
+  const data = await request.formData();
+  const file: File | null = data.get("file") as unknown as File;
+
+  if (!data)
+    return NextResponse.json({ msg: "Form Data Required" }, { status: 400 });
+
+  if (!file)
+    return NextResponse.json({ msg: "Image File required" }, { status: 400 });
+
+  try {
+    const uploadData = await pinata.upload.file(file);
+    const url = await pinata.gateways.convert(uploadData.IpfsHash);
+
+    const imageCID = url.split("/").pop();
+    const imageIPFSURL = "http://ipfs.io/ipfs/" + imageCID;
+
+    return NextResponse.json(imageIPFSURL, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- frontend/app/dashboard/create-nft/page.tsx
+++ frontend/app/dashboard/create-nft/page.tsx
@@ -1,17 +1,92 @@
-'use client'
-import { useState } from "react";
+"use client";
+import { useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { IoCloudUploadOutline } from "react-icons/io5";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import classNames from "classnames";
 
 export default function CreateNft() {
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [name, setName] = useState<string>("");
+  const [ticker, setTicker] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      setSelectedImage(e.target.files[0]);
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
     }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.type.substring(0, 5) === "image") {
+      setSelectedImage(URL.createObjectURL(file));
+      setImageFile(file);
+    } else {
+      setSelectedImage(null);
+      alert("Please select an image file");
+    }
+  };
+
+  const resetInputs = () => {
+    setName("");
+    setTicker("");
+    setDescription("");
+    setSelectedImage(null);
+    setImageFile(null);
+
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
+  const deployNFT = async () => {
+    if (!name) {
+      alert("Please enter a name!");
+      return;
+    }
+
+    if (!ticker) {
+      alert("Please enter a ticker!");
+      return;
+    }
+
+    if (!description) {
+      alert("Please enter a Description!");
+      return;
+    }
+
+    if (!imageFile) {
+      alert("Please upload an image!");
+      return;
+    }
+
+    try {
+      const data = new FormData();
+      data.set("file", imageFile as File);
+
+      const uploadRequest = await fetch("/api/files", {
+        method: "POST",
+        body: data,
+      });
+
+      if (!uploadRequest.ok) {
+        throw new Error("Uploading the file failed.");
+      }
+
+      const imageURI = await uploadRequest.json();
+    } catch (error) {
+      console.log(error);
+      alert(
+        "Trouble uploading the file to IPFS. Please contact the website owner."
+      );
+    }
+
+    // Call createNewNFT() from NFTFactory Contract
+    // Display Deployed NFT Contract Address
   };
 
   return (
@@ -23,74 +98,86 @@ export default function CreateNft() {
           information.
         </h3>
       </div>
-      <div className="flex flex-col items-center md:flex-row gap-32 mt-8">
-        <div className="w-full md:w-1/2">
-          <label
-            htmlFor="imageUpload"
-            className="w-full h-96 border-2 border-dashed border-gray-600 rounded-lg cursor-pointer flex items-center justify-center text-white"
-          >
-            {selectedImage ? (
-              <img
-                src={URL.createObjectURL(selectedImage)}
-                alt="Selected"
-                className="w-full h-full object-cover rounded-lg"
+      <div className="flex flex-row md:flex-row gap-12 mt-8">
+        <div
+          className={classNames(
+            "w-1/2 rounded-xl flex justify-center items-center hover:cursor-pointer relative",
+            {
+              "border-2 border-dashed": !selectedImage,
+            }
+          )}
+          onClick={handleClick}
+        >
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            accept="image/*"
+            style={{ display: "none" }}
+            required
+          />
+          {selectedImage ? (
+            <div className="w-full h-full relative">
+              <Image
+                src={selectedImage}
+                alt="Uploaded image"
+                fill
+                style={{ objectFit: "cover" }}
+                className="rounded-xl"
               />
-            ) : (
-              <div className="flex flex-col items-center gap-2">
-                <div className=""><IoCloudUploadOutline /></div>
-                <div className="text-gray-400">Choose or drag and drop an image here</div>
-              </div>
-            )}
-            <input
-              id="imageUpload"
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="hidden"
-            />
-          </label>
+            </div>
+          ) : (
+            <div className="text-white">Click to upload an image</div>
+          )}
         </div>
         <div className="w-full md:w-1/2">
           <div className="mb-8">
-            <label htmlFor="name" className="text-white">Name</label>
+            <label htmlFor="name" className="text-white">
+              Name
+            </label>
             <Input
               id="name"
               type="text"
-              placeholder="Name you NFT"
+              placeholder="Name your NFT"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="bg-transparent border-gray-600 text-white hover:border-gray-400 mt-2"
             />
           </div>
-          
+
           <div className="mb-8">
-            <label htmlFor="supply" className="text-white">Supply</label>
+            <label htmlFor="ticker" className="text-white">
+              Ticker
+            </label>
             <Input
-              id="supply"
+              id="ticker"
               type="text"
-              placeholder="1"
+              placeholder="NFT Ticker"
+              value={ticker}
+              onChange={(e) => setTicker(e.target.value)}
               className="bg-transparent border-gray-600 text-white hover:border-gray-400 mt-2"
             />
           </div>
-          
+
           <div className="mb-8">
-            <label htmlFor="desc" className="text-white">Description</label>
+            <label htmlFor="desc" className="text-white">
+              Description
+            </label>
             <Textarea
               id="desc"
               placeholder="Enter a description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
               className="bg-transparent border-gray-600 text-white hover:border-gray-400 mt-2"
             />
           </div>
-          
-          <div className="mb-8">
-            <label htmlFor="link" className="text-white">External link</label>
-            <Input
-              id="link"
-              type="link"
-              placeholder="https://examplelink/home"
-              className="bg-transparent border-gray-600 text-white hover:border-gray-400 mt-2"
-            />
-          </div>
-          <div className="flex">
-            <Button className="bg-gray-700">Create</Button>
+          <div className="flex gap-2">
+            <Button className="bg-gray-700" onClick={() => resetInputs()}>
+              Reset
+            </Button>
+            <Button className="bg-gray-700" onClick={() => deployNFT()}>
+              Create
+            </Button>
           </div>
         </div>
       </div>
